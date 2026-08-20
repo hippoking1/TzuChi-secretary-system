88f74ca62144985b435d5c2f90e3b71edf89cfbc
unknown <hippoking1@gmail.com>
2026-08-20 10:46:54 +0800

    chore: set global functions region to asia-east1 (Taiwan)

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
+const { setGlobalOptions } = require('firebase-functions/v2');
 const admin = require('firebase-admin');
+
+// 全域指定部署至台灣機房 (彰化 asia-east1)
+setGlobalOptions({ region: 'asia-east1' });
+
 admin.initializeApp();
 
 const { lineWebhook } = require('./lineWebhook');
